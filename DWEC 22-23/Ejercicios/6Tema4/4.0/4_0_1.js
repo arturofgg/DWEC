dffdd6a9197bdf836e2d39217076974905951512
arturofgg <arturofdezgg17@gmail.com>
2022-11-11 20:41:35 +0100

    hola

---
+++ DWEC 22-23/Ejercicios/6Tema4/4.0/4_0_1.js
@@ -0,0 +1,28 @@
+/*
+1.- La empresa Dolty S.A., tiene tres delegaciones en tres provincias andaluzas
+(Sevilla, Huelva, Cadiz). Cada una de ellas tiene un numero variado de
+empleados, que no es inferior a 4 en ninguna de ellas. Realiza un programa que
+cargue el sueldo de los empleados, junto a su nombre, de cada una de las delegaciones en una array
+distinto. Para finalizar la carga de cada array se ingresa como dato el cero.
+Una vez ingresados los sueldos, mostrar los siguientes datos:
+    a) Informar del tamaño (numero de empleados) de cada una de las delegaciones.
+    b) Determinar la media de los sueldos de cada una de las delegaciones.
+    c) Ordenar de menor a mayor los sueldos de Huelva, De mayor a menor los de Sevilla y
+         Cádiz. Indicando a que delegación pertenece cada lista ordenada
+    d) Determinar  el sueldo menor y el mayor de las tres delegaciones.
+*/
+
+function cargarNombreSueldo(){
+    let arraydiv1 = [];
+    let nombre;
+    let sueldo;
+
+    for(let i=0; arrayN[i] != 0 || arrayS[i] != 0; i++){
+        nombre=prompt("Escribe el nombre del empleado");
+        sueldo=prompt("Escribe el sueldo del empleado");
+        arraydiv1 {
+            
+        }
+    }
+
+}
