09f2b9e0e5fffcb39b4faddb708ed5ab0f4c8ac5
arturofgg <arturofdezgg17@gmail.com>
2022-11-14 21:04:13 +0100

    4.0

--- DWEC 22-23/Ejercicios/6Tema4/4.0/4_0_1.js
+++ DWEC 22-23/Ejercicios/6Tema4/4.0/4_0_1.js
@@ -12,17 +12,64 @@ Una vez ingresados los sueldos, mostrar los siguientes datos:
     d) Determinar  el sueldo menor y el mayor de las tres delegaciones.
 */
 
-function cargarNombreSueldo(){
-    let arraydiv1 = [];
-    let nombre;
-    let sueldo;
+let Huelva = [];
+let Sevilla = [];
+let Cadiz = [];
+let arrayTotal = [];
 
-    for(let i=0; arrayN[i] != 0 || arrayS[i] != 0; i++){
-        nombre=prompt("Escribe el nombre del empleado");
-        sueldo=prompt("Escribe el sueldo del empleado");
-        arraydiv1 {
-            
-        }
-    }
-
+function elegirNumempleados(cadena){
+    let numEmpleados
+    do{
+        numEmpleados =  parseInt(prompt("ESCRIBE EL NUMERO DE EMPLEADOS PARA "+ cadena));
+    }while (numEmpleados<1);
+    return numEmpleados;
 }
+
+function CrearArray(delegacion){
+    let arrayN=[];
+    let arrayS=[];
+    let opcion = delegacion;
+    numEmpleados = elegirNumempleados(opcion);
+    for(let i=0; i<numEmpleados; i++){
+        arrayN[i]=prompt("ESCRIBE EL NOMBRE DEL EMPLEADO");
+        arrayS[i]= parseInt(prompt("ESCRIBE EL SUELDO DEL EMPLEADO"));
+    }
+    return {
+        nombres:arrayN,
+        sueldos:arrayS,
+        numero:numEmpleados
+    }
+}
+
+function mediaSueldos(arraySueldos){
+    let total=0;
+    let media=0;
+    for(let i=0; i<arraySueldos.length; i++ ){
+       total = total + parseInt(arraySueldos[i]); 
+       console.log("Este es el total "+total);
+    }
+    console.log(total);
+    media=parseInt((total/(arraySueldos.length)));
+    return media;
+}
+
+Huelva = CrearArray("HUELVA");
+Sevilla = CrearArray("SEVILLA")
+Cadiz = CrearArray("CADIZ");
+
+document.write("Cantidad de trabajadores en Huelva: " + Huelva.numero + "<br>");
+document.write("Cantidad de trabajadores en Sevilla: " + Sevilla.numero + "<br>");
+document.write("Cantidad de trabajadores en Cadiz: " + Cadiz.numero + "<br><br>");
+
+document.write("La media de sueldos en Huelva es " + mediaSueldos(Huelva.sueldos) + "<br>");
+document.write("La media de sueldos en Sevilla es " + mediaSueldos(Sevilla.sueldos) + "<br>");
+document.write("La media de sueldos en Cadiz es " + mediaSueldos(Cadiz.sueldos) + "<br><br>");
+
+document.write("El orden de los sueldos en Huelva de menor a mayor es " + Huelva.sueldos.sort() + "<br>");
+document.write("El orden de los sueldos en Sevilla de menor a mayor es " + Sevilla.sueldos.sort() + "<br>");
+document.write("El orden de los sueldos en Cadiz de menor a mayor es " + Cadiz.sueldos.sort()  + "<br><br>");
+
+arrayTotal = (Huelva.sueldos.concat(Sevilla.sueldos, Cadiz.sueldos));
+arrayTotal.sort();
+
+document.write("El sueldo menor total es " + arrayTotal[0] + " y el sueldo mayor total es " + arrayTotal[arrayTotal.length-1]  + "<br>");
